386daf1bbe081170594581caca258563dc9e938f
emrekorkmazceng <emre.korkmaz.ceng@gmail.com>
2024-01-04 15:43:34 +0300

    bugs fixed

--- ex0407/ex0407.cpp
+++ ex0407/ex0407.cpp
@@ -1,6 +1,8 @@
-#include<stdio.h>
-int main() {
-//	puts("The secret password is:");
-	puts("Spatula.");
-	return 0;
+#include <stdio.h>
+
+int main()
+{
+    //  puts("The secret password is:");
+    puts("Spatula.");
+    return(0);
 }
